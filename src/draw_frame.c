fb38f51dab0b47c6524f20ad718d1d8fe0659d61
PCHANUL <cpak@student.42seoul.kr>
2022-08-10 23:44:55 +0900

    Implement dda_algo

--- src/draw_frame.c
+++ src/draw_frame.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_frame.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: cpak <cpak@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 10:34:02 by cpak              #+#    #+#             */
-/*   Updated: 2022/08/08 17:42:44 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/08/10 14:16:46 by cpak             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,36 @@ static void	init_frame(t_game *game)
 	mlx_clear_window(game->mlx, game->win);
 	erase_minimap_dirline();
 	erase_minimap_fov();
-	erase_minimap_ray();
-	erase_minimap_player();
+	// erase_minimap_ray();
+	// erase_minimap_player();
 }
 
 void	set_frame(t_game *game)
 {
-	if (game->player.delta.x != 0)
-		game->player.pos.x += game->player.delta.x;
-	if (game->player.delta.y != 0)
-		game->player.pos.y += game->player.delta.y;
-	if (game->player.delta.angle != 0)
-		rotate_player(game, game->player.delta.angle);
+	t_player	*player;
+	t_point		origin_pos;
+
+	player = &(game->player);
+
+	// set grid pos
+	origin_pos.x = player->pos.x - (player->grid_pos.x * MINIMAP_TILE_SIZE);
+	origin_pos.y = player->pos.y - (player->grid_pos.y * MINIMAP_TILE_SIZE);
+	if (origin_pos.x + player->delta.x < 0)
+		player->grid_pos.x -= 1;
+	if (origin_pos.x + player->delta.x > MINIMAP_TILE_SIZE)
+		player->grid_pos.x += 1;
+	if (origin_pos.y + player->delta.y < 0)
+		player->grid_pos.y -= 1;
+	if (origin_pos.y + player->delta.y > MINIMAP_TILE_SIZE)
+		player->grid_pos.y += 1;
+
+	// set pos
+	if (player->delta.x != 0)
+		player->pos.x += player->delta.x;
+	if (player->delta.y != 0)
+		player->pos.y += player->delta.y;
+	if (player->delta.angle != 0)
+		rotate_player(game, player->delta.angle);
 }
 
 int	draw_frame(void)
@@ -40,9 +58,9 @@ int	draw_frame(void)
 	set_frame(game); // calculate player position and direction
 	// draw frame
 	put_minimap();
-	draw_minimap_ray();
 	draw_minimap_dirline();
 	draw_minimap_fov();
-	draw_minimap_player();
+	draw_minimap_ray();
+	// draw_minimap_player();
 	return (0);
 }
--- src/draw_minimap_player.c
+++ src/draw_minimap_player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_minimap_player.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: cpak <cpak@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 10:28:45 by cpak              #+#    #+#             */
-/*   Updated: 2022/08/08 17:48:31 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/08/10 23:43:19 by cpak             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,94 @@ int	draw_minimap_fov(void)
 	return (0);
 }
 
+void	dda_algo(t_vector ray, char **map, t_point grid_pos)
+{
+	// delta.x: abs(광선길이 / ray.x), delta.y: abs(광선길이 / ray.y)
+	// player에서 ray까지 거리를 1로 정하고, 모두 비율로 계산한다.
+	// 그러므로 delta.x: abs(1 / ray.x), delta.y: abs(1 / ray.y)
+
+	t_vector	delta;
+	delta.x = fabsf(1 / ray.x);
+	delta.y = fabsf(1 / ray.y);
+
+	// 지도상의 위치
+	t_point	pos;
+	pos = grid_pos;
+
+	// 초기값
+	t_point		step;
+	t_vector	side;
+
+	if (ray.x < 0)
+	{
+		step.x = -1;
+		side.x = (pos.x - pos.x) * delta.x;
+	}
+	else
+	{
+		step.x = 1;
+		side.x = (pos.x + 1 - pos.x) * delta.x;
+	}
+	if (ray.y < 0)
+	{
+		step.y = -1;
+		side.y = (pos.y - pos.y) * delta.y;
+	}
+	else
+	{
+		step.y = 1;
+		side.y = (pos.y + 1 - pos.y) * delta.y;
+	}
+
+	while (1)
+	{
+		if (side.x < side.y)
+		{
+			side.x += delta.x;
+			pos.x += step.x;
+		}
+		else
+		{
+			side.y += delta.y;
+			pos.y += step.y;
+		}
+		if (map[pos.y][pos.x] == MAP_WALL)
+		{
+			printf("delta.x: %f, delta.y: %f\n", delta.x, delta.y);
+			printf("side.x: %f, side.y: %f\n", side.x, side.y);
+			printf("pos.x: %d, pos.y: %d\n", pos.x, pos.y);
+			return;
+		}
+	}
+}
+
 int	draw_minimap_ray(void)
 {
 	t_game		*game;
-	t_vector	end;
-	int			i;
+	// t_vector	end;
+	// int			i;
+	t_vector	ray;
 
 	game = get_game_struct();
-	i = 0;
-	while (i <= 20)
-	{
-		end.x = game->player.pos.x + (game->player.rays[i].x * 5);
-		end.y = game->player.pos.y + (game->player.rays[i].y * 5);
-		draw_line(game->player.pos, end, MINIMAP_RAY_COLOR);
-		i += 1;
-	}
+	ray.x = game->player.dir.x / MINIMAP_TILE_SIZE;
+	ray.y = game->player.dir.y / MINIMAP_TILE_SIZE;
+
+	dda_algo(ray, game->map.array, game->player.grid_pos);
+
+	// end.x = game->player.pos.x + (game->player.rays[0].x * 5);
+	// end.y = game->player.pos.y + (game->player.rays[0].y * 5);
+
+	// draw_line(game->player.pos, end, MINIMAP_RAY_COLOR);
+
+	// i = 0;
+	// while (i <= 20)
+	// {
+	// 	end.x = game->player.pos.x + (game->player.rays[i].x * 5);
+	// 	end.y = game->player.pos.y + (game->player.rays[i].y * 5);
+
+	// 	draw_line(game->player.pos, end, MINIMAP_RAY_COLOR);
+	// 	i += 1;
+	// }
 	return (0);
 }
 
