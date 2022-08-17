de3fc75c69decc1fd791fc68ee510b248e6257fc
PCHANUL <cpak@student.42seoul.kr>
2022-08-17 15:45:52 +0900

    Fix set_player

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: cpak <cpak@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 17:33:46 by cpak              #+#    #+#             */
-/*   Updated: 2022/08/17 15:14:13 by cpak             ###   ########seoul.kr  */
+/*   Updated: 2022/08/17 15:45:38 by cpak             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,8 @@ void	set_player(void)
 	t_vector	pos;
 
 	game = get_game_struct();
+	pos.x = 0;
+	pos.y = 0;
 	if (game->player.key.move[0])
 	{
 		pos.x = game->player.pos.x + game->player.dir.x / 5;
