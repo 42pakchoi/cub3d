f639bd9070c39032ac989c27d499cc4b5ef50767
srngch <srngch.dev@gmail.com>
2022-08-29 15:40:10 +0900

    Fix bracket pair

--- src/put_minimap_tiles.c
+++ src/put_minimap_tiles.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   put_minimap_tiles.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpak <cpak@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 02:53:00 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/08/29 03:41:47 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/08/29 15:39:42 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ void put_minimap_tiles(void)
 			tile_end.x = tile_start.x + map->minimap_tile_size;
 			tile_end.y = tile_start.y + map->minimap_tile_size;
 			if (map->array[i.y][i.x] == MAP_WALL)
-			{
 				put_image_rect(game->minimap, tile_start, tile_end, MINIMAP_WALL_COLOR);
 			if (map->array[i.y][i.x] == MAP_DOOR)
 				put_image_rect(game->minimap, tile_start, tile_end, MINIMAP_DOOR_COLOR);
